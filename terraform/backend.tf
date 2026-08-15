419bc2cbfadc3e0e6558cff4fff8e1f32be078d8
Gnanateja <gnanateja@gmail.com>
2026-08-15 10:06:18 +0530

    terraform

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "CredPayRG"
-    storage_account_name = "credpaysas1"
-    container_name       = "statefile"
-    key                  = "credpay.terraform.tfstate"
-  }
-}
